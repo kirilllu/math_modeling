b232f1a2f374f308efde3e5582aa006aafd2003e
kirilllu <114501136+kirilllu@users.noreply.github.com>
2022-10-04 16:42:19 +0000

    dfvbxcnvbxcnvjln

---
+++ lec_1_list.py
@@ -0,0 +1,23 @@
+a = [1, 3, 6]
+print(a[0])
+
+b = [8, 10, 11]
+
+c = a + b
+print(c)
+
+# c = a - b
+
+print( c * 2)
+
+c.append('Good')
+print(c)
+
+a.append(b)
+print(a)
+
+print(len(a))
+
+print(len(a[3]))
+
+print((a[3][2]))
